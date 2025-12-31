4d75e6452a1b0784120bd95183a6cae535b8d4cd
Sahin Mallick <sahinmallick14@gmail.com>
2025-12-31 10:02:23 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,20 @@ app.use(
   })
 );
 
-app.options("*", cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
+  
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); 
+  
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
+  next();
+});
+
 
 
 app.use(express.json());
